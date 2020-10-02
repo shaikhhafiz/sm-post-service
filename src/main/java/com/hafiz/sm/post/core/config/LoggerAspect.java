a79712bb342d7ea8c1fdb010e1f9bf66e6a02bb6
Hafiz Ahmed <rhafizubd@gmail.com>
2020-10-02 07:14:26 +0600

    Added logging to file configuration

--- src/main/java/com/hafiz/sm/post/core/config/LoggerAspect.java
+++ src/main/java/com/hafiz/sm/post/core/config/LoggerAspect.java
@@ -1,18 +1,20 @@
 package com.hafiz.sm.post.core.config;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
-@Slf4j
 @Aspect
 @Configuration
 @RequiredArgsConstructor
 public class LoggerAspect {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @AfterThrowing(pointcut = "execution(* com.hafiz.sm.post.*.*.*(..))", throwing = "ex")
     public void logBasePackageError(Exception ex) {
